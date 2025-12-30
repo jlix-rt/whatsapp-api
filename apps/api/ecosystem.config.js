b4b8f8f484f2e75048536e1ec34d5b62aeafa78b
José Reynoso <69912375+ReynosoTiu@users.noreply.github.com>
2025-12-29 23:46:04 -0600

    a

--- apps/api/ecosystem.config.js
+++ apps/api/ecosystem.config.js
@@ -1,14 +1,21 @@
 module.exports = {
-    apps: [
-      {
-        name: 'whatsapp-crunchypaws',
-        script: 'dist/index.js',
-        env: {
-          NODE_ENV: 'production',
-          STORE_ID: 'crunchypaws',
-          PORT: 3333
-        }
-      }
-    ]
-  };
+  apps: [
+    {
+      name: 'whatsapp-api',
+      script: '/var/www/apps/whatsapp-api/index.js',
+      cwd: '/var/www/apps/whatsapp-api',
+      env: {
+        NODE_ENV: 'production',
+        PORT: 3333
+      },
+      instances: 1,
+      exec_mode: 'fork',
+      autorestart: true,
+      max_memory_restart: '500M',
+      error_file: '/var/log/pm2/whatsapp-api-error.log',
+      out_file: '/var/log/pm2/whatsapp-api-out.log',
+      log_date_format: 'YYYY-MM-DD HH:mm:ss Z'
+    }
+  ]
+};
   
